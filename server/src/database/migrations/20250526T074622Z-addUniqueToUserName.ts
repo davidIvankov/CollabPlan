965f7fb8a99d1e0af9b1c0209699c00be385d322
David Ivankov <david.ivankov96@gmail.com>
2025-05-26 10:36:56 +0200

    clean migration

--- server/src/database/migrations/20250526T074622Z-addUniqueToUserName.ts
+++ server/src/database/migrations/20250526T074622Z-addUniqueToUserName.ts
@@ -1,18 +1,11 @@
 import type { Kysely } from 'kysely'
-import { TABLES } from '../dbConstants'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function up(db: Kysely<any>) {
-  await db.schema
-    .createIndex('user_name_unique')
-    .on(TABLES.USER)
-    .column('name')
-    .unique()
-    .execute()
+  // empty migration - no changes
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function down(db: Kysely<any>) {
-  await db.schema
-    .dropIndex('user_name_unique')
-    .ifExists() // optional: avoids error if index was already dropped
-    .execute()
+  // empty migration - no changes
 }
