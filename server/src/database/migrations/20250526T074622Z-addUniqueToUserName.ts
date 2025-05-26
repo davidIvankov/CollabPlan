a640e79ac096e5670415808925f7ee0896fa4165
David Ivankov <david.ivankov96@gmail.com>
2025-05-26 11:29:57 +0200

    retrying failed jobs

--- server/src/database/migrations/20250526T074622Z-addUniqueToUserName.ts
+++ server/src/database/migrations/20250526T074622Z-addUniqueToUserName.ts
@@ -2,10 +2,10 @@ import type { Kysely } from 'kysely'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function up(db: Kysely<any>) {
-  // empty migration - no changes
+  // empty migration - no changes made
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function down(db: Kysely<any>) {
-  // empty migration - no changes
+  // empty migration - no changes made
 }
